34465133f0a1a121c114637f0b59453e1772a325
aastha <asthaturkar1@gmail.com>
2023-06-15 11:35:30 +0530

    Basic Cpp program

---
+++ Basicprogram.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cout<<"Enter a string";
+    getline(cin,s);
+    cout<<"You have entered";
+    cout<<s<<endl;
+    
+    s.push_back('y');
+    cout<<"After using push back"<<s<<endl;
+
+    s.pop_back();
+    cout<<"After using pop back"<<s<<endl;
+
+    return 0;
+}
